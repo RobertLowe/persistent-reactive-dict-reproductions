8a71fda0b037435bf18b2e25e5c9cb080257d585
Robert Lowe <robert@iblargz.com>
2021-03-15 04:51:41 -0400

    Repro for typescript conversion

--- client/main.js
+++ client/main.js
@@ -1,23 +1,25 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
+import { PersistentReactiveDict } from 'meteor/robertlowe:persistent-reactive-dict';
+
 import './main.html';
+
 
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
-  const repro = new PersistentReactiveDict('repro');
-  this.counter = new ReactiveVar(0);
+  this.dict = new PersistentReactiveDict('repro');
 });
 
 Template.hello.helpers({
   counter() {
-    return Template.instance().counter.get();
+    return Template.instance().dict.get('counter');
   },
 });
 
 Template.hello.events({
   'click button'(event, instance) {
     // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
+    instance.dict.set('counter', instance.dict.get('counter') + 1);
   },
 });
